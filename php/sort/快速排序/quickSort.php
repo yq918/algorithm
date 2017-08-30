d7c4a8549f34ffe41f0d31a0c43ccde5f1aa92c1
zhangxuanru <1598484330@qq.com>
2017-08-30 23:34:21 +0800

    add php quick sort

--- php/sort/快速排序/quickSort.php
+++ php/sort/快速排序/quickSort.php
@@ -1,1 +1,31 @@
 <?php
+/*
+快速排序
+*/
+function quickSort($arr){
+ 	$count = count($arr);
+	if($count <= 1){
+		return $arr;
+		}
+ 	$mid = $arr[0];
+	$left = array();
+	$right = array();
+	for($i = 1; $i < $count;$i++){
+		  if($arr[$i] > $mid){
+			  $right[] = $arr[$i];
+		   }else{
+			 $left[]=$arr[$i];
+		    }
+	 }
+		
+		$left  = quickSort($left);
+		$right = quickSort($right);
+ 	   return array_merge($left,array($mid),$right);
+ 	}
+	
+	
+	$arr = array(10,293,4,56,231,65,89,43,67,2);
+	$arr = quickSort($arr);
+	
+	print_r($arr);
+	
