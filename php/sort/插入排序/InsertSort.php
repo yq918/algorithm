22817639759d1095a308e047b12537a8634d0f9c
张选入 <1107099011@qq.com>
2017-10-18 14:55:57 +0800

    Update InsertSort.php

--- php/sort/插入排序/InsertSort.php
+++ php/sort/插入排序/InsertSort.php
@@ -16,9 +16,33 @@ function insertSort($arr)
 		}
  	return $arr;
 }
+
+/*
+插入排序 方法2
+*/
+function insertSort2(array $arr)
+{
+  $count = count($arr);
+  for ($i = 1; $i < $count; $i++) {
+         $val = $arr[$i];
+         $j = $i-1;
+      while ($j >=0 && $arr[$j] > $val) {
+           $arr[$j+1] = $arr[$j];
+           $j--;
+      }
+      if ($j+1 != $i) {
+           $arr[$j+1] = $val;
+      }
+  }
+    return $arr;
+}
+ 
 $arr = array(10,56,78,43,49,23,8,3,68);
 $array = insertSort($arr);
 echo '<pre>';
 print_r($array);
+
+
+
 
 
